08b1efa917d92f30a9c0de685618c0403e1937c5
Tim Griffith <grifmang@gmail.com>
2025-06-14 18:58:29 -0400

    fixes

--- src/pages/ExamSetup.tsx
+++ src/pages/ExamSetup.tsx
@@ -45,7 +45,7 @@ const ExamSetup = () => {
     try {
       // `/api/proxy?url=${encodeURIComponent("https://ytshorts-grif.app.n8n.cloud/webhook/process-media")}`
       // const response = await fetch(ENDPOINTS.CREATE_EXAM {
-      const response = await fetch(`/api/proxy?url=${encodeURIComponent(ENDPOINTS.CREATE_EXAM)}` {
+      const response = await fetch(`/api/proxy?url=${encodeURIComponent(ENDPOINTS.CREATE_EXAM)}`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
